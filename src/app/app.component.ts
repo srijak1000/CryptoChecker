7da2b26a450190f8dc3714dbe9d76607367572bd
srijak1000 <srijak.student@gmail.com>
2023-03-03 12:31:00 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CurrencyService } from './service/currency.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  selectedCurrency : string = "INR"
+  title: any;
+
+  constructor(private currencyService: CurrencyService) {}
+  sendCurrency(event:string) {
+    this.currencyService.setCurrency(event)
+  }
+}
+
+
---
+++ src/app/coin-list/coin-list.component.ts
@@ -0,0 +1,64 @@
+import { ApiService } from '../service/api.service';
+import {AfterViewInit, Component, ViewChild, OnInit} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import { Router } from '@angular/router';
+import { CurrencyService } from '../service/currency.service';
+
+
+@Component({
+  selector: 'app-coin-list',
+  templateUrl: './coin-list.component.html',
+  styleUrls: ['./coin-list.component.scss']
+})
+export class CoinListComponent {
+  bannerData: any = [];
+  currency: string = "INR";
+  dataSource!: MatTableDataSource<any>;
+  displayedColumns: string[] = ['symbol','current_price','price_change_percentage_24h','market_cap']
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private api: ApiService, private router: Router, private currencyService: CurrencyService) { }
+
+  ngOnInit(): void {
+    this.getAllTableData();
+    this.getBannerData();
+    this.currencyService.getCurrency()
+    .subscribe(val=>{
+      this.currency = val;
+      this.getAllTableData();
+      this.getBannerData();
+    })
+  }
+  getBannerData() {
+    this.api.getTrendingCurrency(this.currency)
+    .subscribe(res=>{
+      console.log(res);
+      this.bannerData = res;
+    })
+  }
+  getAllTableData() {
+    this.api.getCurrency(this.currency)
+    .subscribe(res=>{
+      console.log(res);
+      this.dataSource = new MatTableDataSource(res);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+      }
+    )
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  gotoDetails(row:any) {
+    this.router.navigate(['coin-detail',row.id])
+  }
+}
